543d5624622c5effa0957aab26dcab386c5ac1e5
Joshua Holtsclaw <Jildsmiles@yahoo.com>
2019-09-20 13:03:14 -0400

    Search functionality added. working out bugs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList";
 import WelcomePage from "./components/WelcomePage";
+import SearchForm from "./components/SearchForm";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import axios from "axios"
 
 export default function App() {
+
+  const [chars , setChars] = useState([])
+  useEffect(() => {
+    // TODO: Add API Request here - must run in `useEffect`
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(response =>{
+        console.log("response",response.data.results);
+        setChars(response.data.results);
+      })
+  }, []);
+  
+
   return (
-    
     <main>
       <Route exact path="/" component={WelcomePage}></Route>
       <Route path="/chars" render={props => <Header {...props}></Header>}></Route>
-      <Route path="/chars" render={props => <CharacterList {...props}></CharacterList>}></Route>
+      <Route path="/chars"  render={props => <SearchForm {...props} items={chars}></SearchForm>}></Route>
+      <Route path="/chars" render={props => <CharacterList {...props} items={chars}></CharacterList>}></Route>
       
     </main>
   );
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,24 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios"
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import CharacterCard from "./CharacterCard"
-export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
-  const [chars , setChars] = useState([])
-
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    axios
-      .get("https://rickandmortyapi.com/api/character/")
-      .then(response =>{
-        console.log("response",response.data.results);
-        setChars(response.data.results);
-      })
-  }, []);
-
+export default function CharacterList(props) {
+  
   return (
+    
     <section className="character-list">
-      {chars.map(item =>{
+      
+      {props.items.map(item =>{
         return <CharacterCard key={item.id} name={item.name} status={item.status} species={item.species}> </CharacterCard>
       })}
     </section>
